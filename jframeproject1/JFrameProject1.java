3638b83d1bda0fb79cda37217cacca6c971c598f
Chamidilshika <158010483+Chamidilshika@users.noreply.github.com>
2024-01-29 18:49:44 +0530

    Add files via upload

---
+++ jframeproject1/JFrameProject1.java
@@ -0,0 +1,17 @@
+
+package jframeproject1;
+
+import javax.swing.JFrame;
+
+public class JFrameProject1 {
+
+    public static void main(String[] args) {
+       
+        JFrame f = new JFrame();
+        
+        f.setSize(400,400);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+    }
+    
+}
